9728f6b42f97ed4f9926ab0e0796992235e8b5c0
Vinicius Raony <viniciusraony@hotmail.com>
2023-04-11 19:30:13 -0300

    Test env

--- assets/js/main.js
+++ assets/js/main.js
@@ -46,7 +46,7 @@ function buscarRotas() {
 // require('dotenv').config();
 
 const script = document.createElement('script');
-script.src = `https://maps.googleapis.com/maps/api/js?key=AIzaSyDk85nEstYPj-JBpd748eoIynqlCre-8hQ&callback=initMap`;
+script.src = 'https://maps.googleapis.com/maps/api/js?key=' + {GOOGLE_MAPS_API_KEY} + '&callback=initMap';
 script.async = true;
 script.defer = true;
 
